8823f70e81f1529fe34310cea71ad887c2d05772
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-01 14:32:43 +0300

    SPRING DEVTOOLS

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -4,8 +4,6 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 /*controller class*/

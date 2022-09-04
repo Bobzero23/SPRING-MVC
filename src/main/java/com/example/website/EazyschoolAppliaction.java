38760b7286b30a64a330323b0e309fe82e523923
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-04 09:37:45 +0300

    WEBSITE PROJECT

--- src/main/java/com/example/website/EazyschoolAppliaction.java
+++ src/main/java/com/example/website/EazyschoolAppliaction.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EazyschoolAppliaction {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EazyschoolAppliaction.class, args);
 	}
 
 }

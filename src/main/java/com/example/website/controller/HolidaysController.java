e7d0b6b08f969bf02c2d984cb3de7ba068df2cde
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-08 13:38:38 +0300

    REQUEST PARAM PRACTICAL - LITTLE FIX

--- src/main/java/com/example/website/controller/HolidaysController.java
+++ src/main/java/com/example/website/controller/HolidaysController.java
@@ -2,6 +2,7 @@ package com.example.website.controller;
 
 /*imports*/
 import com.example.website.model.Holiday;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 public class HolidaysController {
 

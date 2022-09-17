144f20c5aa5a06a367a13d6c19ba42cf9bddb057
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-17 10:13:05 +0300

    MORNING REVIEW

--- src/main/java/com/example/website/EazyschoolApplication.java
+++ src/main/java/com/example/website/EazyschoolApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EazyschoolAppliaction {
+public class EazyschoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EazyschoolAppliaction.class, args);
+		SpringApplication.run(EazyschoolApplication.class, args);
 	}
 
 }

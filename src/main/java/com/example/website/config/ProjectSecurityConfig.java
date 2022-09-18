71777ca5f4fdd8a60bcf9b10f344cff6e2069477
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-18 17:54:49 +0300

    SPRING SECURITY - COSTUME SECURITY CONFIGURATION FAIL

--- src/main/java/com/example/website/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/website/config/ProjectSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.http.MatcherType.mvc;
+
 
 @Configuration
 public class ProjectSecurityConfig  {
@@ -16,14 +18,24 @@ public class ProjectSecurityConfig  {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
 
         /*Allowing the page to be accessible*/
-        http.authorizeRequests().anyRequest().permitAll().
-                and().formLogin().and().httpBasic();
+        /*http.authorizeRequests().anyRequest().permitAll().
+                and().formLogin().and().httpBasic(); */
 
 
         /*Denying the page to be accessible*/
         /*http.authorizeRequests().anyRequest().denyAll().
                 and().formLogin().and().httpBasic();*/
 
+        /*Customizing the security configuration*/
+        http.csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").authenticated()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
 
         return http.build();
     }

3092086a88746e7beb245bb348278259d02017bb
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-18 16:44:47 +0300

    SPRING SECURITY - DENYING ALL

--- src/main/java/com/example/website/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/website/config/ProjectSecurityConfig.java
@@ -14,8 +14,16 @@ public class ProjectSecurityConfig  {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+
+        /*Allowing the page to be accessible*/
         http.authorizeRequests().anyRequest().permitAll().
                 and().formLogin().and().httpBasic();
+
+
+        /*Denying the page to be accessible*/
+        /*http.authorizeRequests().anyRequest().denyAll().
+                and().formLogin().and().httpBasic();*/
+
 
         return http.build();
     }
